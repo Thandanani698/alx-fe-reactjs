fb29f584733e0256d3acfc3cf962a57804f1a8e0
Thandanani698 <Thandanani698>
2025-02-13 09:10:56 +0200

    Moved WelcomeMessage.jsx to components folder

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Import WelcomeMessage component
+import WelcomeMessage from "./components/WelcomeMessage.jsx";
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,6 +19,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
+      {/* Add WelcomeMessage component here */}
+      <WelcomeMessage />
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -32,4 +39,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
