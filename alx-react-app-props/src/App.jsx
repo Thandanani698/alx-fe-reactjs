d59d489df111f2f6afdd8bb4f81d79aed61bd843
Thandanani698 <Thandanani698>
2025-02-18 10:29:51 +0200

    Implement context API for user data

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,4 @@
 // src/App.jsx
-import React from "react";
 import ProfilePage from "./components/ProfilePage";
 import { UserProvider } from "./components/UserContext";
 import "./App.css"; // Import the CSS file
--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,9 +1,9 @@
 // src/components/UserDetails.jsx
 import { useContext } from "react";
-import UserContext from "./UserContext";
+import UserContext from "./UserContext"; // Import the UserContext
 
 function UserDetails() {
-  const { user } = useContext(UserContext); // Using useContext to get user data
+  const { user } = useContext(UserContext); // Use the context to get the user data
 
   return (
     <div className="user-info">
