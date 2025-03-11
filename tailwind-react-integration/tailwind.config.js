d422d86cd9ea19baedd77866eb5f723f370b5ab7
Thandanani698 <Thandanani698>
2025-03-11 07:02:42 +0200

    Initial commit with Tailwind CSS setup

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
     content: [
-      "./index.html",
-      "./src/**/*.{js,jsx,ts,tsx}",
+      "./index.html",        // Ensure this path is correct for your HTML
+      "./src/**/*.{js,jsx,ts,tsx}",  // Include all your React files
     ],
+    darkMode: 'media',  // You can change this to 'class' if you want to use dark mode manually
     theme: {
       extend: {},
+    },
+    variants: {
+      extend: {},  // Add any variants you need (like hover, focus, etc.)
     },
     plugins: [],
   }
