69484fe612cc4ed6645b202ea42243f797c5a879
Thandanani698 <Thandanani698>
2025-03-17 07:39:08 +0200

    Update Tailwind CSS configuration and add index.html

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,8 @@
-// tailwind.config.js
 module.exports = {
     content: [
-      './index.html', // This is the correct way to include index.html
-      './src/**/*.{js,jsx,ts,tsx}', // Ensure this includes all your React component files
+      './index.html',  
+      './public/index.html', 
+      './src/**/*.{js,jsx,ts,tsx}', 
     ],
     theme: {
       extend: {},
